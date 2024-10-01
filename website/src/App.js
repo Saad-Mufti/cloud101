78f427e33bd6212b427ce105967333718f20320f
Saad Mufti <saadmuf10@gmail.com>
2024-10-01 17:03:38 -0400

    add env files

--- website/src/App.js
+++ website/src/App.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import React, { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from './components/card';
 import { Button } from './components/button';
